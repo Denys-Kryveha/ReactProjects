eccdd23544ccde2d6b896ef0ca16426d30c0cc6d
Denys-Kryveha <densamsung252@gmail.com>
2022-07-16 17:29:16 +0300

    React-1

--- react-1/src/App.js
+++ react-1/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { FunctionComponent } from './components/FunctionComponent/FunctionComponent';
+import { Modal } from './components/Modal/Modal';
 
 function App() {
+  const [open, setOpen] = React.useState(false);
   return (
     <div>
-      <FunctionComponent />
+      <FunctionComponent setOpen = {setOpen} />
+      <Modal open = {open} setOpen={setOpen} />
     </div>
   );
 }
--- react-1/src/components/FunctionComponent/FunctionComponent.js
+++ react-1/src/components/FunctionComponent/FunctionComponent.js
@@ -1,36 +1,30 @@
 import React, { useState } from 'react';
 import { Button } from '../Button/Button';
-import { Modal } from '../Modal/Modal';
+
 
 import styles from './FunctionComponent.module.scss';
 
-export const FunctionComponent = () => {
+export const FunctionComponent = ({setOpen}) => {
   let [counter, setCounter] = useState(0);
-  let [open, setOpen] = React.useState(false);
+
 
   const onIncrement = () => {
-    openWindows(); 
     setCounter(counter + 1);
   };
-  
+
   const onDecrement = () => {
-    openWindows(); 
     setCounter(counter - 1);
   };
 
-  const openWindows = () => {
-    let open = (counter >= 5 || counter <= -5) ? true : false;
-      setOpen(open);
+  if (counter >= 6 || counter <= -6) {
+    setOpen(true);
   };
 
   return (
     <div>
-      <div>
         <Button onClickProp={onDecrement}>Decrement</Button>
         <Button onClickProp={onIncrement}>Increment</Button>
-      </div>
       <p>{counter}</p>
-      <Modal open={open} setOpen={setOpen} />
     </div>
   );
 };
